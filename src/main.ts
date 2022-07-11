1f17b9943681827cb1111f1057ee5bd484783493
vinh <vu.vinh.van@gmail.com>
2022-07-10 20:02:01 -0700

    mid setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
+const ctx = canvas.getContext('2d');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+console.log(ctx)
+
+
